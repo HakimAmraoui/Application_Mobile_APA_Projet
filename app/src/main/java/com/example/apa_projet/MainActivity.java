cb6b50712f8fe5428dd34c5924a8bff245a810cb
HakimAmraoui <hakim.amraoui@outlook.fr>
2021-05-10 11:10:40 +0200

    Page d'accueil avec bar de navigation

--- app/src/main/java/com/example/apa_projet/MainActivity.java
+++ app/src/main/java/com/example/apa_projet/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.apa_projet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.apa_projet.fragments.FragmentCalendar;
+import com.example.apa_projet.fragments.FragmentHome;
+import com.example.apa_projet.fragments.FragmentSearch;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new FragmentHome();
+                            break;
+                        case R.id.nav_calendar:
+                            selectedFragment = new FragmentCalendar();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new FragmentSearch();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+
+
+            };
 }
